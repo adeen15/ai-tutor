5335d895b78aac85de11e24245eeb34df7ff9e9d
adeen15 <adeenamjad15@gmail.com>
2026-02-12 09:46:58 +0500

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require('path');
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
-app.use(express.static(__dirname));
+app.use(express.static(path.join(process.cwd())));
 
 // --- VIEW ROUTES ---
 app.get('/', (req, res) => {
